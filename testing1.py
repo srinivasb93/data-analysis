be074dc926bb4bf0c699c8799aa49038360946c1
srinivasb93 <75192439+srinivasb93@users.noreply.github.com>
2021-07-31 13:09:00 +0530

    Update testing1.py

--- testing1.py
+++ testing1.py
@@ -1,41 +1,43 @@
-from mftool import Mftool
-import nsepy
-from nsepy import get_history
-from datetime import date,datetime as dt
+print('Hello World')
 
-import sqlalchemy as sa
-import urllib.parse
-import pandas as pd
+# from mftool import Mftool
+# import nsepy
+# from nsepy import get_history
+# from datetime import date,datetime as dt
 
-mf = Mftool()
+# import sqlalchemy as sa
+# import urllib.parse
+# import pandas as pd
 
-#Use this for windows authentication
-params = urllib.parse.quote_plus("DRIVER={SQL Server Native Client 11.0};"
-                                 "SERVER=DESKTOP-MAK81E6\SQLEXPRESS;"
-                                 "DATABASE=MFDATA;"
-                                 "Trusted_Connection=yes")
+# mf = Mftool()
 
-'''
-#Use this for SQL server authentication
-params = urllib.parse.quote_plus("DRIVER={SQL Server Native Client 11.0};"
-                                 "SERVER=dagger;"
-                                 "DATABASE=test;"
-                                 "UID=user;"
-                                 "PWD=password")
-'''
+# #Use this for windows authentication
+# params = urllib.parse.quote_plus("DRIVER={SQL Server Native Client 11.0};"
+#                                  "SERVER=DESKTOP-MAK81E6\SQLEXPRESS;"
+#                                  "DATABASE=MFDATA;"
+#                                  "Trusted_Connection=yes")
 
-#Connection String
-engine = sa.create_engine("mssql+pyodbc:///?odbc_connect={}".format(params))
+# '''
+# #Use this for SQL server authentication
+# params = urllib.parse.quote_plus("DRIVER={SQL Server Native Client 11.0};"
+#                                  "SERVER=dagger;"
+#                                  "DATABASE=test;"
+#                                  "UID=user;"
+#                                  "PWD=password")
+# '''
 
-# Connect to the required SQL Server
-conn=engine.connect()
+# #Connection String
+# engine = sa.create_engine("mssql+pyodbc:///?odbc_connect={}".format(params))
 
-query = "SELECT top 1* FROM dbo.Aditya119533 order by date desc"
-df1 = pd.read_sql_query(query,con=conn,parse_dates=True)
-print(df1)
+# # Connect to the required SQL Server
+# conn=engine.connect()
 
-data = pd.DataFrame.from_dict(mf.get_scheme_quote('119533',as_json=False),orient='index')
+# query = "SELECT top 1* FROM dbo.Aditya119533 order by date desc"
+# df1 = pd.read_sql_query(query,con=conn,parse_dates=True)
+# print(df1)
 
-data = data.T
-print(data)
-print(data.columns)
+# data = pd.DataFrame.from_dict(mf.get_scheme_quote('119533',as_json=False),orient='index')
+
+# data = data.T
+# print(data)
+# print(data.columns)
